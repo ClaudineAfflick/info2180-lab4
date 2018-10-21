ecbc4440f2a9e00c5f15e545a341e37b367300a4
ClaudineAfflick <34015969+ClaudineAfflick@users.noreply.github.com>
2018-10-21 15:17:53 -0500

    question 3

--- maze.js
+++ maze.js
@@ -2,6 +2,8 @@ window.onload=function(){
 
   var boundary1= document.getElementById("boundary1");
   var walls = document.querySelectorAll(".boundary");
+  var end= document.getElementById("end");
+  var status= document.getElementById("status")
 
   boundary1.onmouseover = function(){
     boundary1.setAttribute("class", "boundary youlose");
@@ -10,8 +12,14 @@ window.onload=function(){
     walls[i].onmouseover = function(){
       for (var i = 0; i < walls.length-1; i++) {
         walls[i].setAttribute("class", "boundary youlose");
+        status.innerHTML ="You lose.";
       }
     }
   }
+  end.onmouseover=function(){
+     if(boundary1.getAttribute("class")!="boundary youlose"){
+            status.innerHTML =status.innerHTML="Congratulations!!!! YOU WON!!!!";
+    }
+  }
+
 }
-  
