9934ced7e01d9b065d0faabed3f922d2cb9b91f5
ClaudineAfflick <34015969+ClaudineAfflick@users.noreply.github.com>
2018-10-21 03:37:18 -0500

    question 2

--- maze.js
+++ maze.js
@@ -1,8 +1,17 @@
 window.onload=function(){
 
   var boundary1= document.getElementById("boundary1");
+  var walls = document.querySelectorAll(".boundary");
 
   boundary1.onmouseover = function(){
     boundary1.setAttribute("class", "boundary youlose");
   }
+  for (var i = 0; i < walls.length-1; i++) {
+    walls[i].onmouseover = function(){
+      for (var i = 0; i < walls.length-1; i++) {
+        walls[i].setAttribute("class", "boundary youlose");
+      }
+    }
+  }
 }
+  
