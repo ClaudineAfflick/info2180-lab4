d7b387134f84ab8f20074434da18540d599dbfdf
ClaudineAfflick <34015969+ClaudineAfflick@users.noreply.github.com>
2018-10-21 03:22:43 -0500

    question 1 update

--- maze.js
+++ maze.js
@@ -1,8 +1,8 @@
 window.onload=function(){
-var boundary1= document.getElementById("boundary1");
-boundary1.onmouseover = donttouchwalls;
-}
 
-function donttouchwalls(){
-  this.setAttribute(class,"youlose");
+  var boundary1= document.getElementById("boundary1");
+
+  boundary1.onmouseover = function(){
+    boundary1.setAttribute("class", "boundary youlose");
+  }
 }
